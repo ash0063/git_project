a5e06d4c060adaea7c344cb497a53cf38f6f696b
Ashar Shaikhali <shaikhaliashar75@gmail.com>
2026-06-17 23:35:39 +0530

    config

--- config/db.js
+++ config/db.js
@@ -2,19 +2,8 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 
-console.log("HOST:", process.env.MYSQLHOST);
-console.log("PORT:", process.env.MYSQLPORT);
-console.log("USER:", process.env.MYSQLUSER);
-console.log("DB:", process.env.MYSQLDATABASE);
-
-
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE
-});
+console.log(process.env.MYSQL_URL);
+const db = mysql.createConnection( process.env.MYSQL_URL);
 
 db.connect((err) => {
     if (err) {
